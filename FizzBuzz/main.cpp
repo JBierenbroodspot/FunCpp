a77d368d92874648ced6765bb08a587e9ae3fe9f
Jbierenbroodspot <jim12.brood@gmail.com>
2022-02-22 17:37:02 +0100

    Add FizzBuzzHashMap to main.cpp

--- FizzBuzz/main.cpp
+++ FizzBuzz/main.cpp
@@ -3,8 +3,15 @@
 //
 // Created by jim12 on 22/02/2022.
 //
+#include <iostream>
+
 #include "fizzbuzz.h"
 
 int main() {
+    unsigned int fizzbuzz_count = 15;
+
+    std::cout << std::format("FizzBuzz to {} w/o hashmap", fizzbuzz_count) << std::endl;
     fizzbuzz::FizzBuzz(15);
+    std::cout << std::format("FizzBuzz to {} w/ hashmap", fizzbuzz_count) << std::endl;
+    fizzbuzz::FizzBuzzHashMap(15);
 }
