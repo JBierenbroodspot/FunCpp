b5b3980b74ed03e0f535ac9fa6e4f1680b3ed966
Jbierenbroodspot <jim12.brood@gmail.com>
2022-02-23 01:13:31 +0100

    Add empty implementations

---
+++ Trees/src/tree.cpp
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: BSD 3-Clause
+// Copyright (c) 2022 Jimmy Bierenbroodspot.
+//
+// Created by jim12 on 22/02/2022.
+//
+
+#include "tree.h"
+
+namespace trees {
+
+Tree::Tree(trees::TreeTop top, trees::TreeBase base) {}
+
+void Tree::build() const {}
+
+} // namespace trees
---
+++ Trees/src/tree_top.cpp
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: BSD 3-Clause
+// Copyright (c) 2022 Jimmy Bierenbroodspot.
+//
+// Created by jim12 on 22/02/2022.
+//
+
+#include "tree_top.h"
+
+namespace trees {
+
+TreeTop::TreeTop(unsigned int max_width, unsigned int max_height) {}
+
+void TreeTop::build() const {}
+
+} // namespace trees
