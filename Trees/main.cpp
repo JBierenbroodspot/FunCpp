bd599243cb3091b161689718799b8044efaf6a55
Jbierenbroodspot <jim12.brood@gmail.com>
2022-02-23 15:24:59 +0100

    Add entry point

---
+++ Trees/main.cpp
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: BSD 3-Clause
+// Copyright (c) 2022 Jimmy Bierenbroodspot.
+//
+// Created by jim12 on 23/02/2022.
+//
+
+#include "tree.h"
+
+int main() {
+    auto tree = trees::Tree(3, 4, 15);
+    tree.build();
+}
