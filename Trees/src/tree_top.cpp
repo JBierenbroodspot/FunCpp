df9fac489ddbc16c9d58656c122f8e12110c3806
Jbierenbroodspot <jim12.brood@gmail.com>
2022-02-23 12:00:04 +0100

    Implement TreeTop

--- Trees/src/tree_top.cpp
+++ Trees/src/tree_top.cpp
@@ -3,13 +3,24 @@
 //
 // Created by jim12 on 22/02/2022.
 //
+#include <string>
+#include <iostream>
+#include <cmath>
 
 #include "tree_top.h"
 
 namespace trees {
 
-TreeTop::TreeTop(unsigned int max_width, unsigned int max_height) {}
+TreeTop::TreeTop(unsigned int max_width, unsigned int max_height) {
+    this->max_width = max_width;
+    this->max_height = max_height;
+}
 
-void TreeTop::build() const {}
+void TreeTop::build() const {
+    for (int i = (int)floor((double)this->max_width / 2); i >= 0; i--) {
+        unsigned int even_offset = this->max_width % 2 == 0 ? 1 : 0;
+        std::cout << std::string(i, ' ') << std::string(this->max_width - (2 * i) + even_offset, '*') << std::endl;
+    }
+}
 
 } // namespace trees
